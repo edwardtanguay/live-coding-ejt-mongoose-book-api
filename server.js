8a0dc6a8b5ae645ecee4911688d754b026ca2230
Edward Tanguay (windows) <edward@tanguay.info>
2022-07-05 11:20:58 +0200

    POST /book finished

--- server.js
+++ server.js
@@ -7,19 +7,18 @@ mongoose.connect('mongodb://localhost/bookapi');
 const app = express();
 const port = 3459;
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
 	res.send(`<h1>Book API</h1>`);
 });
 
 app.post('/book', async (req, res) => {
-	const book = new Book({
-		title: 'ttt',
-		description: 'ddd',
-		numberOfPages: 999
-	});
+	const book = new Book(req.body);
 	await book.save();
 	res.status(200).json({
-		"message": "book created"
+		"message": "book created",
+		book
 	});
 });
 
