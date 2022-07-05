383dfa59db3452b287c67207851558aed4148bfe
Edward Tanguay (windows) <edward@tanguay.info>
2022-07-05 09:54:02 +0200

    basic /book post

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import { Book } from './models/Book.js';
 
 mongoose.connect('mongodb://localhost/bookapi');
 
@@ -8,6 +9,12 @@ const port = 3459;
 
 app.get('/', (req, res) => {
 	res.send(`<h1>Book API</h1>`);
+});
+
+app.post('/book', (req, res) => {
+	res.status(200).json({
+		"message": "book created"
+	});
 });
 
 app.listen(port, () => {
