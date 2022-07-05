03445d56bd86b420f4184796dfd467f92af6e6b0
Edward Tanguay (windows) <edward@tanguay.info>
2022-07-05 11:55:24 +0200

    PATCH /book/id works

--- server.js
+++ server.js
@@ -51,6 +51,18 @@ app.put('/book/:id', async (req, res) => {
     });
 });
 
+app.patch('/book/:id', async (req, res) => {
+    const id = req.params.id;
+	const oldBook = await Book.find({ _id: id });
+    await Book.updateOne({ _id: id }, {$set: {...req.body}});
+	const newBook = await Book.find({ _id: id });
+    res.status(200).json({
+        message: 'patched book with id=' + id,
+		oldBook,
+		newBook
+    });
+});
+
 app.listen(port, () => {
     console.log(`listening on port: http://localhost:${port}`);
 });
